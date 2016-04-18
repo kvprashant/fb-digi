7bd14cb9aba973d7a4c5815921edc1620c1ed28d
Prashant <kv.prshnt@gmail.com>
2016-04-19 02:54:18 +0530

    Adds visualization of page insights
    Styles the visualization page
    Adds scripts for visualization functionality

--- resources/assets/js/visualize.js
+++ resources/assets/js/visualize.js
@@ -1,1 +1,103 @@
-alert('Loaded');
+function httpGetAsync(theUrl, callback)
+{
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+  if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+    callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+  xmlHttp.send(null);
+}
+
+function logn(position) {
+  // position will be between 0 and 100
+  var minp = 0;
+  var maxp = 500;
+
+  // The result should be between 100 an 500000
+  var minv = Math.log10(100);
+  var maxv = Math.log10(10000);
+
+  // calculate adjustment factor
+  var scale = (maxv-minv) / (maxp-minp);
+
+  return Math.exp(minv + scale*(position-minp));
+}
+
+function getBaseUrl() {
+    var re = new RegExp(/^.*\//);
+    return re.exec(window.location.href)[0];
+}
+
+function loadMap(response) {
+  console.log(response);
+}
+
+function getColor() {
+  return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+var countries,
+    likes,
+    pageLikes = [];
+
+//Loads countries ISO codes
+httpGetAsync(getBaseUrl()+'countries.json', function(response){
+  countries = JSON.parse(response);
+});
+
+// Getting the container's dimensions
+var width = document.getElementById('container').offsetWidth;
+var height = document.getElementById('container').offsetHeight;
+
+//Data
+httpGetAsync(getBaseUrl()+'/api/v1/page_insight?page=297779776717', function(response){
+  likes = JSON.parse(response);
+  // Datamap constructor, fill in the options from the docs
+  var worldMap = new Datamap({
+    element: document.getElementById('container'),
+    geographyConfig: {
+      highlightOnHover: false,
+      popupOnHover: false
+    },
+    fills: {
+      YES: '#666666',
+      UNKNOWN: 'rgb(0,0,0)',       // These are
+      defaultFill: '#CDCCCC'       // the colours
+    },
+    setProjection: function(element, options) {
+      var projection, path;
+      projection = d3.geo.mercator()                          // The d3 projection
+                     .translate([(width/2.2), (height/2)])      // And some options
+                     .scale( width / 2.2 / Math.PI)
+                     .center([-15.652173913043478, 17.2734596919573]);
+      path = d3.geo.path()
+               .projection( projection );
+      return {path: path, projection: projection};
+    }
+  });
+
+  for (var alpha2 in likes) {
+    // console.log([countries[alpha2].alpha3, likes[alpha2], Math.round(100*Math.log(likes[alpha2]))].join(' | '));
+    if(!countries.hasOwnProperty(alpha2)) {
+      continue;
+    }
+    pageLikes.push({
+      name: countries[alpha2].country,
+      radius: Math.round(2*Math.log(likes[alpha2])),
+      likes: likes[alpha2],
+      country: countries[alpha2].alpha3,
+      fillKey: 'YES',
+      centered: countries[alpha2].alpha3
+    });
+  }
+
+  //draw bubbles for likes
+  worldMap.bubbles(pageLikes, {
+    popupTemplate: function (geo, data) {
+      return ['<div class="hoverinfo">' +  data.name,
+      '<br/>Likes: ' +  data.likes + '',
+      '</div>'].join('');
+    }
+  });
+});
--- resources/views/visualization.blade.php
+++ resources/views/visualization.blade.php
@@ -4,16 +4,37 @@
   <title>KPN Facebook page country-wise fans</title>
 
   <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
   <script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/topojson/1.6.9/topojson.min.js"></script>
-  {{ Html::script('js/datamaps.world.min.js') }}
-  {{ Html::script('js/visualize.js') }}
 </head>
 <body>
-  <div class="container">
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1531057830531826',
+        xfbml      : true,
+        version    : 'v2.6'
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  </script>
+  
+  <div id="container">
     <div class="content">
-      <div class="title">Laravel 5</div>
+      <div class="title">KPN Facebook page country-wise fans</div>
+      <p>This page visualizes the fans of <a href="https://www.facebook.com/KPN" target="_blank">KPN's Facebook page</a> across the world</p>
     </div>
   </div>
+  {{ Html::script('js/d3.min.js') }}
+  {{ Html::script('js/datamaps.world.min.js') }}
+  {{ Html::script('js/app.min.js') }}
 </body>
 </html>
